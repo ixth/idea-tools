8906fef835c01cc8910efac0dff6b9f35ce069fb
Mikhail Menshikov <m.menshikov@hh.ru>
2018-02-08 17:20:09 +0300

    Add ssh patching

--- install.sh
+++ install.sh
@@ -11,8 +11,59 @@ add_global_gitignore() {
 }
 
 ignore_tools() {
-    if ! grep -Fxq "hh-dev-tools/" "$global_gitignore"; then
+    if ! grep -Fxq "^hh-dev-tools/" "$global_gitignore"; then
         echo "hh-dev-tools/" >> "$global_gitignore"
+    fi
+}
+
+get_stand_subdomain() {
+    local _stand
+    read -p "Stand (just subdomain, i.e. 'bear', 'ts99'): " _stand
+
+    if ! nslookup "$_stand" 1>/dev/null; then
+        if ! prompt "Can't resolve $stand.pyn.ru, are you sure there's no mistake?"; then
+            exit 1
+        fi
+    fi
+
+    if test -n "$1"; then
+        eval "$1=\$_stand"
+    fi
+}
+
+prompt() {
+    local yn
+    while true; do
+        read -p "$1 [y/n] " yn
+        case $yn in
+            [Yy]* ) return 0; break;;
+            [Nn]* ) return 1; exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+add_ssh_config() {
+   if ! prompt "I'm going to add 'dev' and 'tunnel' host aliases to your ssh config, ok?"; then
+       return
+   fi
+
+    local stand
+    get_stand_subdomain stand
+
+    ssh_dir="$HOME/.ssh"
+    if ! test -d "$ssh_dir"; then
+        mkdir "$ssh_dir"
+    fi
+
+    if ! grep -xq "^Host dev" "$HOME/.ssh/config"; then
+        sed '1s/^/\
+/; s/%/'$stand'/' ssh/dev >> "$HOME/.ssh/config"
+    fi
+
+    if ! grep -xq "^Host tunnel" "$HOME/.ssh/config"; then
+        sed '1s/^/\
+/; s/%/'$stand'/' ssh/tunnel >> "$HOME/.ssh/config"
     fi
 }
 
@@ -26,11 +77,12 @@ copy_tools() {
 build_idea_config() {
     (cd tools && \
     zip -qr ../tools.jar * && \
-    echo 'Now import tools.jar via `File > Import Settings` menu in IntelliJ IDEA')
+    echo "Now import tools.jar via 'File > Import Settings' menu in IntelliJ IDEA")
 }
 
 add_global_gitignore
 ignore_tools
+add_ssh_config
 copy_tools "mhh" `osascript choose-folder.js mhh 2>/dev/null` "hhmobile/__init__.py"
 copy_tools "xhh" `osascript choose-folder.js xhh 2>/dev/null` "xhh/__init__.py"
 build_idea_config
