ef8fe77983b27de4b9f8540b202c6f69be0814c8
Mikhail Menshikov <m.menshikov@hh.ru>
2019-10-22 16:52:57 +0300

    Don't overwrite trl file

--- configs/common/create_trl.sh
+++ configs/common/create_trl.sh
@@ -3,7 +3,10 @@
 . .env-common --source-only
 . .env-local --source-only
 
-cat > "$PROJECT_ROOT/$TRL_PATH/$BRANCH-trans.sql" <<EOF
+TRL_FILE="$PROJECT_ROOT/$TRL_PATH/$BRANCH-trans.sql"
+if [ ! -e "$TRL_FILE" ]; then
+    cat > "$TRL_FILE" <<EOF
 /* Use \u00a0 as &nbsp; */
 select insertTranslation('key', '');
 EOF
+fi
